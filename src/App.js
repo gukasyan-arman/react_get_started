376a694945f883665f60c8ac7d553038bbd86282
gukasyan-arman <gukasian.a.a@gmail.com>
2022-11-04 16:22:30 +0400

    posts app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import "./styles/App.css";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/ PostFilter";
+import MyModal from "./components/UI/myModal/MyModal";
+import { usePosts } from "./components/hooks/usePosts";
+import axios, { Axios } from "axios";
+import PostService from "./api/PostService";
+import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./components/hooks/useFetching";
+import { getPageCount, getPagesArr } from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [posts, setPosts] = useState([])
+	const [filter, setFilter] = useState({sort: "", query: ""})
+	const [modal, setModal] = useState(false)
+	const [totalPages, setTotalPages] = useState(0)
+	const [limit, setLimit] = useState(10)
+	const [page, setPage] = useState(1)
+	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+	const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
+		const response = await PostService.getAll(limit, page);
+		setPosts(response.data)
+		const totalCount = response.headers['x-total-count']
+		setTotalPages(getPageCount(totalCount, limit))
+	})
+
+	console.log(totalPages)
+
+	useEffect(() => {
+		fetchPosts(limit, page)
+	}, [page]) 
+
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost])
+		setModal(false)
+	}
+
+	const removePost = (post) => {
+		setPosts(posts.filter(p => p.id !== post.id))
+	}
+
+	const changePage = (page) => {
+		setPage(page)
+	}
+
+	return (
+		<div className="App">
+			<button onClick={fetchPosts} >GET POSTS</button>
+			<MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+				Создать пост
+			</MyButton>
+			<MyModal visible={modal} setVisible={setModal}>
+				<PostForm create={createPost} />
+			</MyModal>
+			<hr style={{margin: "15px 0"}} /> 
+			<PostFilter
+				filter={filter} setFilter={setFilter}
+			/>
+			{postError && 
+				<h1>Произошла ошибка: {postError}</h1>
+			}
+			{isPostLoading
+				? 
+				<div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
+					<Loader />
+				</div>
+				: <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Spisok"}/>
+			}
+			<Pagination
+				page={page} 
+				changePage={changePage}
+				totalPages={totalPages}
+			/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/ PostFilter.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import MyInput from './UI/input/MyInput';
+import MySelect from './UI/select/MySelect';
+
+function PostFilter({filter, setFilter}) {
+	return (
+		<div>
+			<MyInput
+				placeholder="Поиск..."
+				value={filter.query}
+				onChange={e => setFilter({...filter, query: e.target.value})}
+			/>
+			<MySelect 
+				value={filter.sort}
+				onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
+				defaultValue="Сортировка"
+				options={[
+					{value: "title", name: "По названию"},
+					{value: "body", name: "По описанию"},
+					{value: "id", name: "По умолчанию"}
+				]}
+			/>
+		</div>
+	);
+}
+
+export default PostFilter;
